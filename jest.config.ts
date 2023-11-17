5beaa2795d1f35aa21ac29b68a4dc579dedef4e2
Arie Trouw <arie.trouw@xylabs.com>
2023-11-17 10:06:31 -0800

    tests now work (required resolutions of quick-lru)

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,14 @@ const generateJestConfig = ({ esModules }: { esModules: string[] }) => {
   const esModulesList = Array.isArray(esModules) ? esModules.join('|') : esModules
   return {
     coveragePathIgnorePatterns: ['<rootDir>/(.*)/dist'],
+    globalSetup: './jestSetup/globalSetup.ts',
+    globalTeardown: './jestSetup/globalTeardown.ts',
     moduleNameMapper: {
       '^(\\.{1,2}/.*)\\.js$': '$1',
     },
     preset: 'ts-jest/presets/default-esm',
     setupFiles: ['dotenv/config'],
-    setupFilesAfterEnv: ['jest-sorted', 'jest-extended/all'],
+    setupFilesAfterEnv: ['jest-sorted', 'jest-extended/all', './jestSetup/setupFiles.ts'],
     testRegex: '(/__tests__/.*|(\\.|/)((!perf\\.)test|spec))\\.tsx?$',
     testTimeout: 200000,
     transform: {
@@ -23,7 +25,7 @@ const generateJestConfig = ({ esModules }: { esModules: string[] }) => {
   }
 }
 
-const config = generateJestConfig({ esModules: ['is-ip', 'ip-regex', 'lodash-es', 'uuid', 'lodash-es'] })
+const config = generateJestConfig({ esModules: ['is-ip', 'ip-regex', 'lodash-es', 'uuid', 'lodash-es', 'quick-lru'] })
 
 // eslint-disable-next-line import/no-default-export
 export default config
