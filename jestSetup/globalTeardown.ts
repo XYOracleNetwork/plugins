465191e8aebfb78bc2ab4d05b81d95d10bf4ce0a
Arie Trouw <arie.trouw@xylabs.com>
2023-11-17 10:50:04 -0800

    updo

--- jestSetup/globalTeardown.ts
+++ jestSetup/globalTeardown.ts
@@ -6,6 +6,4 @@ import { Config } from 'jest'
  * Jest global teardown method runs after all tests are run
  * https://jestjs.io/docs/configuration#globalteardown-string
  */
-module.exports = async (_globalConfig: Config, _projectConfig: Config) => {
-
-}
+module.exports = async (_globalConfig: Config, _projectConfig: Config) => {}
