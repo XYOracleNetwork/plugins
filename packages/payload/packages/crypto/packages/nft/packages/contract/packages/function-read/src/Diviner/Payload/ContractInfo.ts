d852b80bfaf97ff87d7330caf46092f0a4021587
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-07 18:18:25 -0600

    Add identity function for contract info

--- packages/payload/packages/crypto/packages/nft/packages/contract/packages/function-read/src/Diviner/Payload/ContractInfo.ts
+++ packages/payload/packages/crypto/packages/nft/packages/contract/packages/function-read/src/Diviner/Payload/ContractInfo.ts
@@ -1,4 +1,4 @@
-import { Payload } from '@xyo-network/payload-model'
+import { isPayloadOfSchemaType, Payload } from '@xyo-network/payload-model'
 
 /**
  * The schema for the contract info
@@ -31,3 +31,11 @@ export interface SmartContractInfo<T extends Record<string, unknown> = Record<st
  * The smart contract info payload
  */
 export type ContractInfo = Payload<SmartContractInfo, ContractInfoSchema>
+
+/**
+ * Checks if the payload is a crypto contract info payload
+ *
+ * @param payload The payload to check
+ * @returns True if the payload is a contract info payload
+ */
+export const isCryptoContractInfo = isPayloadOfSchemaType<ContractInfo>(ContractInfoSchema)
