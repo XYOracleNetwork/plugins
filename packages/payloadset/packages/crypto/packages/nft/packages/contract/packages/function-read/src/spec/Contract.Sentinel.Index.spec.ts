0111a9aaacf836120c91639ae7146d0b5aac8d38
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-07 20:51:52 -0600

    Add tests for payload schema mapping in sentinel

--- packages/payloadset/packages/crypto/packages/nft/packages/contract/packages/function-read/src/spec/Contract.Sentinel.Index.spec.ts
+++ packages/payloadset/packages/crypto/packages/nft/packages/contract/packages/function-read/src/spec/Contract.Sentinel.Index.spec.ts
@@ -1,16 +1,16 @@
 /* eslint-disable max-statements */
 import { HDWallet } from '@xyo-network/account'
 import {
-  ContractInfo,
-  ContractInfoSchema,
   CryptoContractFunctionCall,
   CryptoContractFunctionCallSchema,
+  isErc721ContractInfo,
+  isErc1155ContractInfo,
 } from '@xyo-network/crypto-contract-function-read-payload-plugin'
 import { JsonPatchDiviner } from '@xyo-network/diviner-jsonpatch'
 import { ManifestWrapper, PackageManifestPayload } from '@xyo-network/manifest'
 import { ModuleFactory, ModuleFactoryLocator } from '@xyo-network/module-model'
+import { MemoryNode } from '@xyo-network/node-memory'
 import { ERC721__factory, ERC721Enumerable__factory, ERC1155__factory } from '@xyo-network/open-zeppelin-typechain'
-import { isPayloadOfSchemaType } from '@xyo-network/payload-model'
 import { asSentinelInstance } from '@xyo-network/sentinel-model'
 import { getProviderFromEnv } from '@xyo-network/witness-blockchain-abstract'
 import { Provider } from 'ethers'
@@ -21,9 +21,11 @@ import manifestFile from './Contract.Sentinel.Index.json'
 
 const maxProviders = 32
 
-describe('Erc721Sentinel', () => {
-  const address = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D' // Bored Apes
-  // const address = '0x7daec605e9e2a1717326eedfd660601e2753a057' // 10ktf-stockroom
+describe('Sentinel', () => {
+  const cases: [string, string][] = [
+    ['With ERC-721', '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'],
+    // ['With ERC-1155', '0xCaf94eB06D4dE233c45B353723C387D3E440f3d6'],
+  ]
 
   const getProviders = () => {
     const providers: Provider[] = []
@@ -34,12 +36,13 @@ describe('Erc721Sentinel', () => {
   }
 
   describe('report', () => {
-    it('specifying address', async () => {
+    let wallet: HDWallet
+    let node: MemoryNode
+    beforeAll(async () => {
       const mnemonic = 'later puppy sound rebuild rebuild noise ozone amazing hope broccoli crystal grief'
-      const wallet = await HDWallet.fromPhrase(mnemonic)
+      wallet = await HDWallet.fromPhrase(mnemonic)
       const locator = new ModuleFactoryLocator()
       locator.register(CryptoContractDiviner)
-
       locator.register(
         new ModuleFactory(CryptoContractFunctionReadWitness, {
           config: { abi: ERC721__factory.abi },
@@ -47,7 +50,6 @@ describe('Erc721Sentinel', () => {
         }),
         { 'network.xyo.crypto.contract.interface': 'Erc721' },
       )
-
       locator.register(
         new ModuleFactory(CryptoContractFunctionReadWitness, {
           config: { abi: ERC721Enumerable__factory.abi },
@@ -55,7 +57,6 @@ describe('Erc721Sentinel', () => {
         }),
         { 'network.xyo.crypto.contract.interface': 'Erc721Enumerable' },
       )
-
       locator.register(
         new ModuleFactory(CryptoContractFunctionReadWitness, {
           config: { abi: ERC1155__factory.abi },
@@ -65,14 +66,26 @@ describe('Erc721Sentinel', () => {
       )
       locator.register(JsonPatchDiviner)
       const manifest = new ManifestWrapper(manifestFile as PackageManifestPayload, wallet, locator)
-      const node = await manifest.loadNodeFromIndex(0)
+      node = await manifest.loadNodeFromIndex(0)
+    })
+    it.each(cases)('%s', async (_, address) => {
       const collectionSentinel = asSentinelInstance(await node.resolve('NftInfoSentinel'))
       expect(collectionSentinel).toBeDefined()
       const collectionCallPayload: CryptoContractFunctionCall = { address, schema: CryptoContractFunctionCallSchema }
       const report = await collectionSentinel?.report([collectionCallPayload])
-      const info = report?.find(isPayloadOfSchemaType(ContractInfoSchema)) as ContractInfo | undefined
-      expect(info?.results?.name).toBe('BoredApeYachtClub')
-      expect(info?.results?.symbol).toBe('BAYC')
+      let foundAny = false
+      const erc721 = report?.find(isErc721ContractInfo)
+      if (erc721) {
+        foundAny = true
+        expect(erc721?.results?.name).toBe('BoredApeYachtClub')
+        expect(erc721?.results?.symbol).toBe('BAYC')
+      }
+      const erc1155 = report?.find(isErc1155ContractInfo)
+      if (erc1155) {
+        foundAny = true
+        expect(erc1155?.results?.url).toBeDefined()
+      }
+      expect(foundAny).toBe(true)
     })
   })
 })
