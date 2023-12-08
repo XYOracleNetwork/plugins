375394f250f53926c34012a746f91b16a545b10a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-07 18:03:01 -0600

    Add diviner labels

---
+++ packages/payload/packages/crypto/packages/nft/packages/contract/packages/function-read/src/Diviner/Labels.ts
@@ -0,0 +1,15 @@
+import { Labels } from '@xyo-network/module-model'
+
+/**
+ * Labels for Crypto Contract Diviner components
+ */
+export interface CryptoContractDivinerLabels extends Labels {
+  'network.xyo.crypto.contract.info': 'diviner'
+}
+
+/**
+ * Labels for Crypto Contract Diviner components
+ */
+export const CryptoContractDivinerLabels: CryptoContractDivinerLabels = {
+  'network.xyo.crypto.contract.info': 'diviner',
+}
