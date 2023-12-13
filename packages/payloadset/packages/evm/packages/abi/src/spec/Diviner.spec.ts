e23d6c2fa2f79689d9bb567c3e362c8b086524de
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-13 07:54:17 -0600

    Add red/green test cases

--- packages/payloadset/packages/evm/packages/abi/src/spec/Diviner.spec.ts
+++ packages/payloadset/packages/evm/packages/abi/src/spec/Diviner.spec.ts
@@ -5,16 +5,36 @@ import { ERC20__factory } from '@xyo-network/open-zeppelin-typechain'
 import { EvmAbiImplementedDiviner, EvmAbiImplementedDivinerConfigSchema } from '../Diviner'
 
 describeIf(process.env.INFURA_PROJECT_ID)('EvmAbiImplementedDiviner', () => {
-  const address = '0x55296f69f40ea6d20e478533c15a6b08b654e758' //XYO ERC20
+  const cases = [
+    ['0x55296f69f40ea6d20e478533c15a6b08b654e758', ERC20__factory.abi], //XYO ERC20
+  ] as const
   describe('divine', () => {
     describe('with matching ABI', () => {
-      it('get code from contract', async () => {
+      it.each(cases)('returns implemented true', async (address, abi) => {
         const witness = await EvmAbiImplementedDiviner.create({
           account: 'random',
-          config: { abi: ERC20__factory.abi, schema: EvmAbiImplementedDivinerConfigSchema },
+          config: { abi, schema: EvmAbiImplementedDivinerConfigSchema },
         })
         const observation = await witness.divine([{ address, block: 0, chainId: 1, code: '0x00', schema: EvmContractSchema }])
-        expect(observation[0].address).toBe(address)
+        expect(observation?.length).toBeGreaterThan(0)
+        for (const obs of observation ?? []) {
+          expect(obs.address).toBe(address)
+          expect(obs.implemented).toBeTrue()
+        }
+      })
+    })
+    describe('without matching ABI', () => {
+      it.each(cases)('returns implemented false', async (address, abi) => {
+        const witness = await EvmAbiImplementedDiviner.create({
+          account: 'random',
+          config: { abi, schema: EvmAbiImplementedDivinerConfigSchema },
+        })
+        const observation = await witness.divine([{ address, block: 0, chainId: 1, code: '0x00', schema: EvmContractSchema }])
+        expect(observation?.length).toBeGreaterThan(0)
+        for (const obs of observation ?? []) {
+          expect(obs.address).toBe(address)
+          expect(obs.implemented).toBeFalse()
+        }
       })
     })
   })
