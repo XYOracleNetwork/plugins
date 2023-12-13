5e0ccf65fd82cd953ad3f60ac14b7796280bfe85
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-13 08:21:47 -0600

    Tests for additional interfaces

--- packages/payloadset/packages/evm/packages/abi/src/spec/Diviner.spec.ts
+++ packages/payloadset/packages/evm/packages/abi/src/spec/Diviner.spec.ts
@@ -1,13 +1,28 @@
 import { describeIf } from '@xylabs/jest-helpers'
 import { EvmContractSchema, EvmContractWitness, EvmContractWitnessConfigSchema } from '@xyo-network/evm-contract-witness'
-import { ERC20__factory } from '@xyo-network/open-zeppelin-typechain'
+import {
+  ERC20__factory,
+  ERC721__factory,
+  ERC1155__factory,
+  ERC1155Burnable__factory,
+  ERC1155Holder__factory,
+  ERC1155URIStorage__factory,
+} from '@xyo-network/open-zeppelin-typechain'
 import { BlockchainAddressSchema, getProvidersFromEnv } from '@xyo-network/witness-blockchain-abstract'
 
 import { EvmAbiImplementedDiviner, EvmAbiImplementedDivinerConfigSchema } from '../Diviner'
 
 describeIf(process.env.INFURA_PROJECT_ID)('EvmAbiImplementedDiviner', () => {
   const cases = [
-    ['0x55296f69f40ea6d20e478533c15a6b08b654e758', ERC20__factory.abi], //XYO ERC20
+    ['0x55296f69f40ea6d20e478533c15a6b08b654e758', ERC20__factory.abi], // XYO ERC20
+    ['0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D', ERC721__factory.abi], // BAYC
+    ['0xEdB61f74B0d09B2558F1eeb79B247c1F363Ae452', ERC1155Holder__factory.abi], // Gutter Cat Gang
+    ['0x2A6d6a082C410a195157EC4caf67CB9fD718f087', ERC1155Holder__factory.abi], // Spider Tanks
+    // ['0x33FD426905F149f8376e227d0C9D3340AaD17aF1', ERC1155URIStorage__factory.abi], // The Memes by 6529
+    // ['0x33FD426905F149f8376e227d0C9D3340AaD17aF1', ERC1155__factory.abi], // The Memes by 6529
+    // ['0x33FD426905F149f8376e227d0C9D3340AaD17aF1', ERC1155Burnable__factory.abi], // The Memes by 6529
+    // ['0x33FD426905F149f8376e227d0C9D3340AaD17aF1', ERC1155Holder__factory.abi], // The Memes by 6529
+    // ['0x33FD426905F149f8376e227d0C9D3340AaD17aF1', ERC1155URIStorage__factory.abi], // The Memes by 6529
   ] as const
   describe('divine', () => {
     describe('with matching ABI', () => {
