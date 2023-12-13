5cc35c8fc33e60f1eda8f3175678d1f229335517
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-13 07:59:43 -0600

    Observe contract during test

--- packages/payloadset/packages/evm/packages/abi/src/spec/Diviner.spec.ts
+++ packages/payloadset/packages/evm/packages/abi/src/spec/Diviner.spec.ts
@@ -1,6 +1,7 @@
 import { describeIf } from '@xylabs/jest-helpers'
-import { EvmContractSchema } from '@xyo-network/evm-contract-witness'
+import { EvmContractSchema, EvmContractWitness, EvmContractWitnessConfigSchema } from '@xyo-network/evm-contract-witness'
 import { ERC20__factory } from '@xyo-network/open-zeppelin-typechain'
+import { getProvidersFromEnv } from '@xyo-network/witness-blockchain-abstract'
 
 import { EvmAbiImplementedDiviner, EvmAbiImplementedDivinerConfigSchema } from '../Diviner'
 
@@ -11,29 +12,37 @@ describeIf(process.env.INFURA_PROJECT_ID)('EvmAbiImplementedDiviner', () => {
   describe('divine', () => {
     describe('with matching ABI', () => {
       it.each(cases)('returns implemented true', async (address, abi) => {
-        const witness = await EvmAbiImplementedDiviner.create({
+        const witness = await EvmContractWitness.create({
+          account: 'random',
+          config: { address, schema: EvmContractWitnessConfigSchema },
+          providers: getProvidersFromEnv,
+        })
+        const diviner = await EvmAbiImplementedDiviner.create({
           account: 'random',
           config: { abi, schema: EvmAbiImplementedDivinerConfigSchema },
         })
-        const observation = await witness.divine([{ address, block: 0, chainId: 1, code: '0x00', schema: EvmContractSchema }])
-        expect(observation?.length).toBeGreaterThan(0)
-        for (const obs of observation ?? []) {
-          expect(obs.address).toBe(address)
-          expect(obs.implemented).toBeTrue()
+        const observations = await witness.observe()
+        expect(observations?.length).toBeGreaterThan(0)
+        const code = observations?.[0].code
+        const results = await diviner.divine([{ address, block: 0, chainId: 1, code, schema: EvmContractSchema }])
+        expect(results?.length).toBeGreaterThan(0)
+        for (const result of results ?? []) {
+          expect(result.address).toBe(address)
+          expect(result.implemented).toBeTrue()
         }
       })
     })
     describe('without matching ABI', () => {
       it.each(cases)('returns implemented false', async (address, abi) => {
-        const witness = await EvmAbiImplementedDiviner.create({
+        const diviner = await EvmAbiImplementedDiviner.create({
           account: 'random',
           config: { abi, schema: EvmAbiImplementedDivinerConfigSchema },
         })
-        const observation = await witness.divine([{ address, block: 0, chainId: 1, code: '0x00', schema: EvmContractSchema }])
-        expect(observation?.length).toBeGreaterThan(0)
-        for (const obs of observation ?? []) {
-          expect(obs.address).toBe(address)
-          expect(obs.implemented).toBeFalse()
+        const results = await diviner.divine([{ address, block: 0, chainId: 1, code: '0x00', schema: EvmContractSchema }])
+        expect(results?.length).toBeGreaterThan(0)
+        for (const result of results ?? []) {
+          expect(result.address).toBe(address)
+          expect(result.implemented).toBeFalse()
         }
       })
     })
