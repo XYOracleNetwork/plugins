a47c1279f58556a3356bfb5805c928d46ebd95a9
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-13 15:34:57 -0600

    Documentation

--- packages/payload/packages/evm/packages/address/src/Payload.ts
+++ packages/payload/packages/evm/packages/address/src/Payload.ts
@@ -2,10 +2,21 @@ import { Payload } from '@xyo-network/payload-model'
 
 import { EvmAddressSchema } from './Schema'
 
-export type EvmAddress = Payload<
-  {
-    address: string
-    chainId: number
-  },
-  EvmAddressSchema
->
+/**
+ * The fields of an EVM address payload
+ */
+export interface EvmAddressFields {
+  /**
+   * An EVM address
+   */
+  address: string
+  /**
+   * The EVM chain ID
+   */
+  chainId: number
+}
+
+/**
+ * A payload that contains an EVM address
+ */
+export type EvmAddress = Payload<EvmAddressFields, EvmAddressSchema>
--- packages/payload/packages/evm/packages/address/src/Schema.ts
+++ packages/payload/packages/evm/packages/address/src/Schema.ts
@@ -1,2 +1,8 @@
+/**
+ * The schema for an EVM address.
+ */
 export type EvmAddressSchema = 'network.xyo.evm.address'
+/**
+ * The schema for an EVM address.
+ */
 export const EvmAddressSchema: EvmAddressSchema = 'network.xyo.evm.address'
