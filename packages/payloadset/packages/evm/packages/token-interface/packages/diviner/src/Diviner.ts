aac47741ce4cea80900055dd512009462b7e9e82
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-14 08:33:11 -0600

    Implement diviner

--- packages/payloadset/packages/evm/packages/token-interface/packages/diviner/src/Diviner.ts
+++ packages/payloadset/packages/evm/packages/token-interface/packages/diviner/src/Diviner.ts
@@ -3,47 +3,89 @@ import { AbstractDiviner } from '@xyo-network/abstract-diviner'
 import { DivinerConfig, DivinerParams } from '@xyo-network/diviner-model'
 import { EvmContract, EvmContractSchema } from '@xyo-network/evm-contract-witness'
 import { AnyConfigSchema } from '@xyo-network/module-model'
+import {
+  ERC20__factory,
+  ERC721__factory,
+  ERC1155__factory,
+  IERC721Enumerable__factory,
+  IERC721Metadata__factory,
+  IERC721Receiver__factory,
+  IERC1155MetadataURI__factory,
+  IERC1155Receiver__factory,
+} from '@xyo-network/open-zeppelin-typechain'
 import { isPayloadOfSchemaType } from '@xyo-network/payload-model'
-import { Interface } from 'ethers'
+import { Interface, JsonFragment } from 'ethers'
 
-import { EvmFunctionImplemented, EvmFunctionImplementedSchema, InterfaceAbi } from './Payload'
+import { EvmTokenInterfaceImplemented, EvmTokenInterfaceImplementedSchema, TokenInterface } from './Payload'
 
 export const EvmTokenInterfaceDivinerConfigSchema = 'network.xyo.evm.token.interface.implemented.diviner.config'
 export type EvmTokenInterfaceDivinerConfigSchema = typeof EvmTokenInterfaceDivinerConfigSchema
 
-export type EvmTokenInterfaceDivinerConfig = DivinerConfig<{ abi?: InterfaceAbi; schema: EvmTokenInterfaceDivinerConfigSchema }>
+export type EvmTokenInterfaceDivinerConfig = DivinerConfig<{ schema: EvmTokenInterfaceDivinerConfigSchema; tokenInterfaces?: TokenInterface[] }>
 
 export type EvmTokenInterfaceDivinerParams = DivinerParams<AnyConfigSchema<EvmTokenInterfaceDivinerConfig>>
+
+type DistributiveMappedType<T> = T extends string ? { [K in T]: readonly JsonFragment[] } : never
+type TokenInterfaceDictionary = DistributiveMappedType<TokenInterface>
 
 export class EvmTokenInterfaceDiviner<TParams extends EvmTokenInterfaceDivinerParams = EvmTokenInterfaceDivinerParams> extends AbstractDiviner<
   TParams,
   EvmContract,
-  EvmFunctionImplemented
+  EvmTokenInterfaceImplemented
 > {
+  /**
+   * The list of supported token interfaces
+   */
+  static readonly SupportedTokenInterfaces: Readonly<Record<TokenInterface, readonly JsonFragment[]>> = {
+    ERC1155: ERC1155__factory.abi,
+    ERC1155Metadata_URI: IERC1155MetadataURI__factory.abi,
+    ERC1155TokenReceiver: IERC1155Receiver__factory.abi,
+    ERC20: ERC20__factory.abi,
+    ERC721: ERC721__factory.abi,
+    ERC721Enumerable: IERC721Enumerable__factory.abi,
+    ERC721Metadata: IERC721Metadata__factory.abi,
+    ERC721TokenReceiver: IERC721Receiver__factory.abi,
+  }
   static override configSchemas = [EvmTokenInterfaceDivinerConfigSchema]
 
-  get abi() {
-    return assertEx(this.config?.abi, 'No ABI specified')
+  private _tokenInterfaces?: TokenInterfaceDictionary
+
+  get tokenInterfaces() {
+    if (!this._tokenInterfaces) {
+      if (this.config?.tokenInterfaces) {
+        this._tokenInterfaces =
+          (Object.fromEntries(
+            this.config?.tokenInterfaces.map((tokenInterface) => {
+              return [tokenInterface, EvmTokenInterfaceDiviner.SupportedTokenInterfaces[tokenInterface]] as const
+            }),
+          ) as TokenInterfaceDictionary) ?? {}
+      } else {
+        this._tokenInterfaces = EvmTokenInterfaceDiviner.SupportedTokenInterfaces
+      }
+    }
+    return this._tokenInterfaces
   }
 
-  protected override async divineHandler(inPayloads: EvmContract[] = []): Promise<EvmFunctionImplemented[]> {
+  protected override async divineHandler(inPayloads: EvmContract[] = []): Promise<EvmTokenInterfaceImplemented[]> {
     await this.started('throw')
     try {
       const observations = await Promise.all(
         inPayloads.filter(isPayloadOfSchemaType<EvmContract>(EvmContractSchema)).map(({ address, code }) => {
-          const contractInterface = new Interface(this.abi)
           const byteCode = assertEx(code, 'Missing code')
 
-          const results: EvmFunctionImplemented[] = []
+          const results: EvmTokenInterfaceImplemented[] = []
 
-          contractInterface.forEachFunction(({ selector }) => {
-            const observation: EvmFunctionImplemented = {
-              address,
-              implemented: byteCode.includes(BigInt(selector).toString(16)),
-              schema: EvmFunctionImplementedSchema,
-              selector,
-            }
-            results.push(observation)
+          Object.entries(this.tokenInterfaces).forEach(([tokenInterface, abi]) => {
+            const contractInterface = new Interface(abi)
+            contractInterface.forEachFunction(({ selector }) => {
+              const observation: EvmTokenInterfaceImplemented = {
+                address,
+                implemented: byteCode.includes(BigInt(selector).toString(16)),
+                schema: EvmTokenInterfaceImplementedSchema,
+                tokenInterface: tokenInterface as TokenInterface,
+              }
+              results.push(observation)
+            })
           })
 
           return results
--- packages/payloadset/packages/evm/packages/token-interface/packages/diviner/src/Payload.ts
+++ packages/payloadset/packages/evm/packages/token-interface/packages/diviner/src/Payload.ts
@@ -1,16 +1,21 @@
 import { Payload } from '@xyo-network/payload-model'
 import { JsonFragment } from 'ethers'
 
-export type InterfaceAbi = ReadonlyArray<JsonFragment>
+export type ERC20TokenInterfaces = 'ERC20'
+export type ERC721TokenInterfaces = 'ERC721' | 'ERC721TokenReceiver' | 'ERC721Metadata' | 'ERC721Enumerable'
+export type ERC1155TokenInterfaces = 'ERC1155' | 'ERC1155TokenReceiver' | 'ERC1155Metadata_URI'
 
-export const EvmFunctionImplementedSchema = 'network.xyo.evm.function.implemented'
-export type EvmFunctionImplementedSchema = typeof EvmFunctionImplementedSchema
+export type TokenInterface = ERC20TokenInterfaces | ERC721TokenInterfaces | ERC1155TokenInterfaces
 
-export type EvmFunctionImplemented = Payload<
+export const EvmTokenInterfaceImplementedSchema = 'network.xyo.evm.token.interface.implemented'
+export type EvmTokenInterfaceImplementedSchema = typeof EvmTokenInterfaceImplementedSchema
+
+export type EvmTokenInterfaceImplemented = Payload<
   {
     address: string
+    chainId: number
     implemented: boolean
-    selector: string
+    tokenInterface: TokenInterface
   },
-  EvmFunctionImplementedSchema
+  EvmTokenInterfaceImplementedSchema
 >
