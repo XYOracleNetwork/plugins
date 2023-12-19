3be97c3f336187fa38e25e2823676ddaf613cce9
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-19 17:48:40 -0600

    Add indexes

--- packages/payloadset/packages/crypto/packages/nft/packages/contract/packages/function-read/src/spec/Erc721.TotalSupply.Index.spec.ts
+++ packages/payloadset/packages/crypto/packages/nft/packages/contract/packages/function-read/src/spec/Erc721.TotalSupply.Index.spec.ts
@@ -30,7 +30,6 @@ import erc721TotalSupplyIndexManifest from './Erc721.TotalSupply.Index.json'
 const maxProviders = 32
 
 describeIf(process.env.INFURA_PROJECT_ID)('Erc721.TotalSupply.Index', () => {
-  const address = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D' //Bored Apes
   let wallet: HDWallet
   let node: MemoryNode
 
@@ -70,12 +69,25 @@ describeIf(process.env.INFURA_PROJECT_ID)('Erc721.TotalSupply.Index', () => {
     ['0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'], // BAYC
   ] as const
   describeIf(process.env.INFURA_PROJECT_ID)('Sentinel', () => {
-    describe('with matching ABI', () => {
+    describe('Sentinel', () => {
       it.each(cases)('returns totalSupply', async (address) => {
+        const sentinel = asSentinelInstance(await node.resolve('Sentinel'))
+        const input = { address, chainId: 1, schema: CryptoContractFunctionCallSchema }
+        const observations = await sentinel?.report([input])
+        expect(observations?.length).toBe(3)
+        const totalSupply = observations?.filter(isCryptoContractFunctionCallResult).find(isCryptoContractFunctionCallSuccess)
+        expect(totalSupply).toBeDefined()
+        expect(totalSupply?.result).toBeString()
+        const totalSupplyValue = totalSupply?.result as string
+        expect(parseInt(totalSupplyValue)).toBeNumber()
+      })
+    })
+    describe('Index', () => {
+      it.each(cases)('returns indexed result', async (address) => {
         const sentinel = asSentinelInstance(await node.resolve('EvmContractSentinel'))
         const input = { address, chainId: 1, schema: CryptoContractFunctionCallSchema }
         const observations = await sentinel?.report([input])
-        expect(observations?.length).toBe(2)
+        expect(observations?.length).toBe(3)
         const totalSupply = observations?.filter(isCryptoContractFunctionCallResult).find(isCryptoContractFunctionCallSuccess)
         expect(totalSupply).toBeDefined()
         expect(totalSupply?.result).toBeNumber()
