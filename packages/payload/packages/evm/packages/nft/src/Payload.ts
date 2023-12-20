875e48c2839395a411f2828c7cebd14b37b7792a
Joel Carter <JoelBCarter@users.noreply.github.com>
2023-12-20 06:10:57 -0600

    Derive NFT Id from Evm Address

--- packages/payload/packages/evm/packages/nft/src/Payload.ts
+++ packages/payload/packages/evm/packages/nft/src/Payload.ts
@@ -1,13 +1,32 @@
+import { EvmAddressFields } from '@xyo-network/evm-address-payload-plugin'
 import { isPayloadOfSchemaType, Payload } from '@xyo-network/payload-model'
 
 import { NftIdSchema } from './Schema'
 
-export interface NftIdFields {
-  address: string
-  chainId: number
+/**
+ * The fields of an NFT ID payload
+ */
+export interface NftIdFields extends EvmAddressFields {
+  // TODO: Add these fields here? Make optional? Derive from this and make another payload?
+  // /**
+  //  * Total supply of the token
+  //  */
+  // supply: string
+  // TODO: type number instead?
+  /**
+   * The token ID
+   */
   tokenId: string
 }
 
+/**
+ * A payload that contains an NFT ID
+ */
 export type NftId = Payload<NftIdFields, NftIdSchema>
 
+/**
+ * Checks if a payload is of type NftId
+ * @param payload The payload to check
+ * @returns True if the payload is of type NftId
+ */
 export const isNftId = isPayloadOfSchemaType<NftId>(NftIdSchema)
