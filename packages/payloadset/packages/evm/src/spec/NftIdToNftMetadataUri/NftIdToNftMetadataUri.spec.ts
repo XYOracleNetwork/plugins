b146724fddf04dc2232f52d49d7898002cf853d8
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-02 14:35:37 -0600

    Working metadata URI Node

--- packages/payloadset/packages/evm/src/spec/NftIdToNftMetadataUri/NftIdToNftMetadataUri.spec.ts
+++ packages/payloadset/packages/evm/src/spec/NftIdToNftMetadataUri/NftIdToNftMetadataUri.spec.ts
@@ -6,7 +6,7 @@ import { asDivinerInstance } from '@xyo-network/diviner-model'
 import { EvmCall, EvmCallDiviner, EvmCallResults, EvmCallResultsSchema, EvmCallSchema, EvmCallWitness } from '@xyo-network/evm-call-witness'
 import { ManifestWrapper, PackageManifestPayload } from '@xyo-network/manifest'
 import { ModuleFactory, ModuleFactoryLocator } from '@xyo-network/module-model'
-import { ERC721__factory, ERC721Enumerable__factory, ERC721URIStorage__factory, ERC1155__factory } from '@xyo-network/open-zeppelin-typechain'
+import { ERC721URIStorage__factory } from '@xyo-network/open-zeppelin-typechain'
 import { isPayloadOfSchemaType } from '@xyo-network/payload-model'
 import { asSentinelInstance } from '@xyo-network/sentinel-model'
 import { getProvidersFromEnv } from '@xyo-network/witness-evm-abstract'
@@ -19,80 +19,45 @@ const profiler = createProfiler()
 const tokenCount = 0
 const maxProviders = 2
 
-describe('Erc721Sentinel - Token', () => {
-  //const address = '0x562fC2927c77cB975680088566ADa1dC6cB8b5Ea' //Random ERC721
+describe('Sentinel', () => {
   const address = '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D' //Bored Apes
-  //const address = '0x495f947276749Ce646f68AC8c248420045cb7b5e' //OpenSea Storefront
-  //const address = '0x6802df79bcbbf019fe5cb366ff25720d1365cfd3' //Upgradeable
-
   const tokenId = 1
-
   const providers = getProvidersFromEnv(maxProviders)
-
   describeIf(providers.length)('report', () => {
-    it('specifying address', async () => {
+    it('Returns metadata for token ID', async () => {
       profile(profiler, 'setup')
       const wallet = await HDWallet.random()
       const locator = new ModuleFactoryLocator()
       locator.register(EvmCallDiviner)
-
-      locator.register(
-        new ModuleFactory(EvmCallWitness, {
-          config: { abi: ERC721__factory.abi },
-          providers: () => getProvidersFromEnv(maxProviders),
-        }),
-        { 'network.xyo.evm.interface': 'Erc721' },
-      )
-
-      locator.register(
-        new ModuleFactory(EvmCallWitness, {
-          config: { abi: ERC721Enumerable__factory.abi },
-          providers: () => getProvidersFromEnv(maxProviders),
-        }),
-        { 'network.xyo.evm.interface': 'Erc721Enumerable' },
-      )
-
       locator.register(
         new ModuleFactory(EvmCallWitness, {
           config: { abi: ERC721URIStorage__factory.abi },
           providers: () => getProvidersFromEnv(maxProviders),
         }),
-        { 'network.xyo.evm.interface': 'ERC721URIStorage' },
+        { 'network.xyo.evm.interface': 'ERC721TokenUri' },
       )
-
-      locator.register(
-        new ModuleFactory(EvmCallWitness, {
-          config: { abi: ERC1155__factory.abi },
-          providers: () => getProvidersFromEnv(maxProviders),
-        }),
-        { 'network.xyo.evm.interface': 'Erc1155' },
-      )
-
       profile(profiler, 'setup')
       profile(profiler, 'manifest')
-      const manifest = new ManifestWrapper(erc721TokenSentinelManifest as PackageManifestPayload, wallet, locator)
+      const manifest = erc721TokenSentinelManifest as PackageManifestPayload
+      const manifestWrapper = new ManifestWrapper(manifest, wallet, locator)
       profile(profiler, 'manifest-load')
-      const node = await manifest.loadNodeFromIndex(0)
+      const node = await manifestWrapper.loadNodeFromIndex(0)
       profile(profiler, 'manifest-load')
       profile(profiler, 'manifest-resolve')
       const mods = await node.resolve()
       profile(profiler, 'manifest-resolve')
       profile(profiler, 'manifest')
-      expect(mods.length).toBeGreaterThan(5)
+      const privateModules = manifest.nodes[0].modules?.private ?? []
+      const publicModules = manifest.nodes[0].modules?.public ?? []
+      expect(mods.length).toBe(privateModules.length + publicModules.length + 1)
 
-      const tokenSentinel = asSentinelInstance(await node.resolve('NftTokenInfoSentinel'))
+      const tokenSentinel = asSentinelInstance(await node.resolve('NftTokenUriSentinel'))
       expect(tokenSentinel).toBeDefined()
 
       const tokenUriWitness = asWitnessInstance(await node.resolve('Erc721TokenURIWitness'))
       expect(tokenUriWitness).toBeDefined()
 
-      const ownerOfWitness = asWitnessInstance(await node.resolve('Erc721OwnerOfWitness'))
-      expect(ownerOfWitness).toBeDefined()
-
-      const uriWitness = asWitnessInstance(await node.resolve('Erc1155UriWitness'))
-      expect(uriWitness).toBeDefined()
-
-      const diviner = asDivinerInstance(await node.resolve('TokenInfoDiviner'))
+      const diviner = asDivinerInstance(await node.resolve('EvmCallDiviner'))
       expect(diviner).toBeDefined()
 
       const tokenCallPayload: EvmCall = { address, args: [tokenId], schema: EvmCallSchema }
@@ -100,7 +65,6 @@ describe('Erc721Sentinel - Token', () => {
       const report = await tokenSentinel?.report([tokenCallPayload])
       const info = report?.find(isPayloadOfSchemaType(EvmCallResultsSchema)) as EvmCallResults | undefined
       console.log(`info: ${JSON.stringify(info, null, 2)}`)
-      expect(info?.results?.['ownerOf']?.result).toBeString()
       expect(info?.results?.['tokenURI']?.result).toBeString()
     })
     afterAll(() => {
