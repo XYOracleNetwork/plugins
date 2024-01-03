defe500a2819a9430356e3d661c91edc1bf8de30
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-03 13:23:28 -0600

    Add diviner tests

--- packages/payloadset/packages/evm/packages/call/packages/token-uri/diviner/src/spec/Diviner.spec.ts
+++ packages/payloadset/packages/evm/packages/call/packages/token-uri/diviner/src/spec/Diviner.spec.ts
@@ -1,32 +1,30 @@
-import { describeIf } from '@xylabs/jest-helpers'
-import { ERC20__factory } from '@xyo-network/open-zeppelin-typechain'
-import { isPayloadOfSchemaType, Payload } from '@xyo-network/payload-model'
-import { getProvidersFromEnv } from '@xyo-network/witness-evm-abstract'
+// eslint-disable-next-line workspaces/no-absolute-imports
+import { EvmCallResults, EvmCallResultsSchema } from '@xyo-network/evm-call-witness'
+import { isNftMetadataUri } from '@xyo-network/evm-nft-id-payload-plugin'
+import { Payload } from '@xyo-network/payload-model'
 
-import { EvmCallDiviner } from '../Diviner'
-import { EvmCall, EvmCallResultSchema, EvmCallSchema } from '../Payload'
+import { EvmCallResultToNftTokenUriDiviner, EvmCallResultToNftTokenUriDivinerConfigSchema } from '../Diviner'
 
-const validateObservation = (observation: Payload[]) => {
-  const results = observation.filter(isPayloadOfSchemaType(EvmCallResultSchema))
+const validateResult = (observation: Payload[]) => {
+  const results = observation.filter(isNftMetadataUri)
   expect(results.length).toBeGreaterThan(0)
   expect(observation.length).toEqual(results.length)
 }
 
-describeIf(process.env.INFURA_PROJECT_ID)('CryptoWalletNftDiviner', () => {
+describe('CryptoWalletNftDiviner', () => {
   const address = '0x55296f69f40ea6d20e478533c15a6b08b654e758' //XYO ERC20
-  const functionName = 'balanceOf'
+  const chainId = 1
   const args = ['0xaDe7DFBC532A01dB67BFEA3b728D4eA22869f381'] //Random Holder
-  describe('observe', () => {
-    describe('with no address or chainId in query', () => {
-      it('uses values from config', async () => {
-        const witness = await EvmCallDiviner.create({
-          account: 'random',
-          config: { schema: EvmCallDivinerConfigSchema },
-        })
-        const call: EvmCall = { address, args, functionName, schema: EvmCallSchema }
-        const observation = await witness.divine([call])
-        validateObservation(observation)
+  const cases = [[address, chainId, { balanceOf: { args, result: '0x0' } }]] as const
+  describe('divine', () => {
+    it.each(cases)('uses values from config', async (address, chainId, results) => {
+      const diviner = await EvmCallResultToNftTokenUriDiviner.create({
+        account: 'random',
+        config: { schema: EvmCallResultToNftTokenUriDivinerConfigSchema },
       })
+      const evmCallResults: EvmCallResults = { address, chainId, results, schema: EvmCallResultsSchema }
+      const result = await diviner.divine([evmCallResults])
+      validateResult(result)
     })
   })
 })
