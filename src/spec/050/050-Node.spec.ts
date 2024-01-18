a403519d948a1d32be81e4b69cde2ce5f957a9e7
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-18 09:06:59 -0600

    Move to subfolders for additional file isolation

--- src/spec/050/050-Node.spec.ts
+++ src/spec/050/050-Node.spec.ts
@@ -67,4 +67,40 @@ describe('Node', () => {
     const describe = await node.describe()
     console.log(describe)
   })
+  it('create from manifest file', async () => {
+    const wallet = await HDWallet.random()
+    const manifest: PackageManifestPayload = {
+      nodes: [
+        {
+          config: {
+            name: 'Node',
+            schema: 'network.xyo.node.config',
+          },
+          modules: {
+            private: [
+              {
+                config: {
+                  name: 'PrivateArchivist',
+                  schema: 'network.xyo.archivist.config',
+                },
+              },
+            ],
+            public: [
+              {
+                config: {
+                  name: 'PublicArchivist',
+                  schema: 'network.xyo.archivist.config',
+                },
+              },
+            ],
+          },
+        },
+      ],
+      schema: 'network.xyo.manifest.package',
+    }
+    const wrapper = new ManifestWrapper(manifest, wallet)
+    const [node] = await wrapper.loadNodes()
+    const describe = await node.describe()
+    console.log(describe)
+  })
 })
