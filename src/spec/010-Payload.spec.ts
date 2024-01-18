3fa09df93b6fb8340ce12ff8133c8621cebdba54
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-01-18 07:57:51 -0600

    Payload hashing examples

--- src/spec/010-Payload.spec.ts
+++ src/spec/010-Payload.spec.ts
@@ -1,5 +1,26 @@
+import { PayloadHasher } from '@xyo-network/hash'
+
 describe('Payload', () => {
-  it('create payload', () => {
-    const payload = { salt: '1', schema: 'network.xyo.id' }
+  describe('create', () => {
+    it('simple ', () => {
+      const payload = { schema: 'network.xyo.payload' }
+      console.log(payload)
+    })
+    it('of schema', () => {
+      const payload = { salt: '1', schema: 'network.xyo.id' }
+      console.log(payload)
+    })
+  })
+  describe('hash', () => {
+    it('hash a payload', () => {
+      const payload = { salt: '1', schema: 'network.xyo.id' }
+      const hash = PayloadHasher.hashSync(payload)
+      console.log(hash)
+    })
+    it('hash a async (async)', async () => {
+      const payload = { salt: '1', schema: 'network.xyo.id' }
+      const hash = await PayloadHasher.hashAsync(payload)
+      console.log(hash)
+    })
   })
 })
