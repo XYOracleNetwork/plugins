ea52be76be20c4a09e108441ca3ed7c8b39d1024
Arie Trouw <arie.trouw@xylabs.com>
2024-03-28 22:36:38 -0700

    updo

--- packages/payloadset/packages/crypto/packages/nft/packages/nft/packages/witness/src/spec/Witness.Index.spec.ts
+++ packages/payloadset/packages/crypto/packages/nft/packages/nft/packages/witness/src/spec/Witness.Index.spec.ts
@@ -149,7 +149,7 @@ describe('CryptoWalletNftWitness Index', () => {
       await Promise.all(
         data.map(async (nft) => {
           const timestamp = { schema: 'network.xyo.timestamp', timestamp: Date.now() }
-          const [bw, payloads] = await (await new BoundWitnessBuilder().payloads([nft, timestamp])).build()
+          const [bw, payloads] = await new BoundWitnessBuilder().payloads([nft, timestamp]).build()
           return [bw, ...payloads]
         }),
       )
