4c36b179541fe11d6459f1099ffada80bb9699b9
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-03 05:07:19 -0500

    Add createEscrowIntentWithSecret to handle signing of secret

--- packages/payload/packages/payments/src/Escrow/createEscrowIntent.ts
+++ packages/payload/packages/payments/src/Escrow/createEscrowIntent.ts
@@ -1,15 +1,29 @@
 import { AccountInstance } from '@xyo-network/account-model'
 import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
+import { IdPayload } from '@xyo-network/id-payload-plugin'
 
 import { EscrowTerms } from './Terms'
 
 /**
  * Creates an escrow intent
+ * @deprecated Use createEscrowIntentWithSecret instead
  * @param terms The payloads describing the terms for the escrow
  * @param account The account to create the escrow intent with
  * @returns The escrow intent
  */
 export const createEscrowIntent = async (terms: EscrowTerms[], account: AccountInstance) => {
   const result = await new BoundWitnessBuilder({ accounts: [account] }).payloads([...terms]).build()
+  return result
+}
+
+/**
+ * Creates an escrow intent (for a buyer or seller) using the supplied secret
+ * @param terms The payload describing the terms for the escrow
+ * @param secret The secret for the escrow principal party to use for the escrow
+ * @param account The account to create the escrow intent with
+ * @returns The escrow intent
+ */
+export const createEscrowIntentWithSecret = async (terms: EscrowTerms, secret: IdPayload, account: AccountInstance) => {
+  const result = await new BoundWitnessBuilder({ accounts: [account] }).payloads([terms, secret]).build()
   return result
 }
