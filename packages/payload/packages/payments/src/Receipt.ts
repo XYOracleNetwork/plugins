e044216ff060f18c102ae4d1ef43c166a829f633
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-05 18:34:50 -0500

    Add receipts

---
+++ packages/payload/packages/payments/src/Receipt.ts
@@ -0,0 +1,42 @@
+import {
+  isPayloadOfSchemaType,
+  isPayloadOfSchemaTypeWithMeta,
+  isPayloadOfSchemaTypeWithSources,
+  PayloadWithSources,
+} from '@xyo-network/payload-model'
+
+import { SupportedCurrency } from './Currency'
+
+export type ReceiptSchema = 'network.xyo.payments.receipt'
+export const ReceiptSchema: ReceiptSchema = 'network.xyo.payments.receipt'
+
+export interface ReceiptFields {
+  /**
+   * The amount paid
+   */
+  amount: number
+  /**
+   * The currency of the amount paid
+   */
+  currency: SupportedCurrency
+}
+
+/**
+ * A receipt is a record of a payment made
+ */
+export type Receipt = PayloadWithSources<ReceiptFields, ReceiptSchema>
+
+/**
+ * Identity function for determine if an object is a Receipt
+ */
+export const isReceipt = isPayloadOfSchemaType<Receipt>(ReceiptSchema)
+
+/**
+ * Identity function for determine if an object is a Receipt with sources
+ */
+export const isReceiptWithSources = isPayloadOfSchemaTypeWithSources<Receipt>(ReceiptSchema)
+
+/**
+ * Identity function for determine if an object is a Receipt with meta
+ */
+export const isReceiptWithMeta = isPayloadOfSchemaTypeWithMeta<Receipt>(ReceiptSchema)
