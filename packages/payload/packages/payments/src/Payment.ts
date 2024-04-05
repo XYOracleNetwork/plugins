7f071a2bc1e4622dd1ba2f0edd573d9f1b12977d
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-05 18:24:56 -0500

    Add Identity functions for additional payment types

--- packages/payload/packages/payments/src/Payment.ts
+++ packages/payload/packages/payments/src/Payment.ts
@@ -1,4 +1,9 @@
-import { isPayloadOfSchemaType, PayloadWithSources } from '@xyo-network/payload-model'
+import {
+  isPayloadOfSchemaType,
+  isPayloadOfSchemaTypeWithMeta,
+  isPayloadOfSchemaTypeWithSources,
+  PayloadWithSources,
+} from '@xyo-network/payload-model'
 
 import { SupportedCurrency } from './Currency'
 
@@ -21,4 +26,17 @@ export interface PaymentFields {
  */
 export type Payment = PayloadWithSources<PaymentFields, PaymentSchema>
 
+/**
+ * Identity function for determine if an object is a Payment
+ */
 export const isPayment = isPayloadOfSchemaType<Payment>(PaymentSchema)
+
+/**
+ * Identity function for determine if an object is a Payment with sources
+ */
+export const isPaymentWithSources = isPayloadOfSchemaTypeWithSources<Payment>(PaymentSchema)
+
+/**
+ * Identity function for determine if an object is a Payment with meta
+ */
+export const isPaymentWithMeta = isPayloadOfSchemaTypeWithMeta<Payment>(PaymentSchema)
