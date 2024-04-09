c06cc8c2d1f80ebcf62ae7f48b1b2e3d9e26b8f4
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-09 12:15:55 -0500

    Add TLD to domain

--- packages/payload/packages/xns/plugins/record/src/Domain/Domain.ts
+++ packages/payload/packages/xns/plugins/record/src/Domain/Domain.ts
@@ -3,11 +3,24 @@ import { isPayloadOfSchemaType, Payload } from '@xyo-network/payload-model'
 export type DomainSchema = 'network.xyo.ns.domain'
 export const DomainSchema: DomainSchema = 'network.xyo.ns.domain'
 
+/**
+ * The supported TLDs for XNS
+ */
+export type SupportedTld = 'xyo'
+
+/**
+ * The fields for a domain record
+ */
 export interface DomainFields {
   /**
    * The XNS domain of the record
    */
   domain: string
+
+  /**
+   * The TLD of the domain
+   */
+  tld: SupportedTld
 }
 
 /**
