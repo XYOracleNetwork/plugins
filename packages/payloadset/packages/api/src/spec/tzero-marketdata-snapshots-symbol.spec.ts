e3c88ca727a7bd07a12b7daaef34eb51aa61f0be
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-28 16:37:04 -0500

    Assert response types

--- packages/payloadset/packages/api/src/spec/tzero-marketdata-snapshots-symbol.spec.ts
+++ packages/payloadset/packages/api/src/spec/tzero-marketdata-snapshots-symbol.spec.ts
@@ -1,4 +1,5 @@
 /* eslint-disable max-statements */
+import { assertEx } from '@xylabs/assert'
 import { describeIf } from '@xylabs/jest-helpers'
 import { JsonObject, toJsonString } from '@xylabs/object'
 import { HDWallet } from '@xyo-network/account'
@@ -16,10 +17,29 @@ import tzeroMarketdataManifest from './tzero-marketdata-snapshots-symbol.json'
 describe('tZero', () => {
   const symbol = 'XYLB'
 
-  const apiKey = process.env.TZERO_MARKETDATA_SANDBOX_API_KEY
+  const apiKey = process.env.TZERO_MARKETDATA_API_KEY
 
   describeIf(apiKey)('snapshots', () => {
-    type TZeroMarketdataSandboxResponse = JsonObject
+    interface TZeroMarketdataSnapshotResponse extends JsonObject {
+      askPrice: number | null
+      askPriceRate: number | null
+      askQtyBookTotal: number
+      askQuantity: number | null
+      bidPrice: number | null
+      bidPriceRate: number | null
+      bidQtyBookTotal: number | null
+      bidQuantity: number | null
+      high: number | null
+      lastPrice: number | null
+      lastQuantity: number | null
+      low: number | null
+      open: number | null
+      prevClosePx: number | null
+      symbol: string
+      timestamp: string
+      volume: number
+    }
+
     it('specifying symbol', async () => {
       const mnemonic = 'later puppy sound rebuild rebuild noise ozone amazing hope broccoli crystal grief'
       const wallet = await HDWallet.fromPhrase(mnemonic)
@@ -52,10 +72,27 @@ describe('tZero', () => {
 
       const report = await sentinel?.report([call])
 
-      const apiCallResult = report?.find(isPayloadOfSchemaType<ApiCallJsonResult<TZeroMarketdataSandboxResponse>>(ApiCallResultSchema))
+      const apiCallResult = report?.find(isPayloadOfSchemaType<ApiCallJsonResult<TZeroMarketdataSnapshotResponse>>(ApiCallResultSchema))
       expect(apiCallResult).toBeDefined()
       console.log(`Result: ${toJsonString(apiCallResult, 10)}`)
       expect(isApiCallErrorResult(apiCallResult)).toBe(false)
+      const data = assertEx(apiCallResult?.data)
+      expect(data.askPrice).toEqual(expect.any(Number))
+      expect(data.askPriceRate).toBeOneOf([null, expect.any(Number)])
+      expect(data.askQtyBookTotal).toEqual(expect.any(Number))
+      expect(data.askQuantity).toEqual(expect.any(Number))
+      expect(data.bidPrice).toEqual(expect.any(Number))
+      expect(data.bidPriceRate).toBeOneOf([null, expect.any(Number)])
+      expect(data.bidQtyBookTotal).toEqual(expect.any(Number))
+      expect(data.bidQuantity).toEqual(expect.any(Number))
+      expect(data.high).toBeOneOf([null, expect.any(Number)])
+      expect(data.lastPrice).toBeOneOf([null, expect.any(Number)])
+      expect(data.lastQuantity).toBeOneOf([null, expect.any(Number)])
+      expect(data.low).toBeOneOf([null, expect.any(Number)])
+      expect(data.open).toBeOneOf([null, expect.any(Number)])
+      expect(data.prevClosePx).toEqual(expect.any(Number))
+      expect(data.symbol).toEqual(expect.any(String))
+      expect(data.timestamp).toEqual(expect.any(String))
     })
   })
 })
