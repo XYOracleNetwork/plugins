86812bdf36f6d6b4460491a9fc29b0e4904b7dc4
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-29 18:09:34 -0500

    Rename package to match parent

--- packages/payload/packages/stock/src/index.ts
+++ packages/payload/packages/stock/src/index.ts
@@ -1,5 +1,1 @@
-export * from '@xyo-network/crypto-cards-payload-plugins'
-export * from '@xyo-network/crypto-market-payload-plugins'
-
-// eslint-disable-next-line import/no-default-export
-export default CryptoPayloadPlugins
+export * from '@xyo-network/stock-market-payload-plugins'
