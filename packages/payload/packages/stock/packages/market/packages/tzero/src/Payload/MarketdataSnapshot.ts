49e1b0e0d5e37454e34bf2104ee057c0272a35f0
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-05-29 14:59:04 -0500

    Use single interface for responses

--- packages/payload/packages/stock/packages/market/packages/tzero/src/Payload/MarketdataSnapshot.ts
+++ packages/payload/packages/stock/packages/market/packages/tzero/src/Payload/MarketdataSnapshot.ts
@@ -1,16 +1,18 @@
-export interface MarketdataSnapshot {
-  askPrice: number
+export interface MarketdataSnapshotFields {
+  askPrice: number | null
   askPriceRate: number | null
-  askQtyBookTotal: number
-  askQuantity: number
-  bidPrice: number
+  askQtyBookTotal: number | null
+  askQuantity: number | null
+  bidPrice: number | null
   bidPriceRate: number | null
+  bidQtyBookTotal: number | null
+  bidQuantity: number | null
   high: number | null
   lastPrice: number | null
   lastQuantity: number | null
   low: number | null
   open: number | null
-  prevClosePx: number
+  prevClosePx: number | null
   symbol: string
   timestamp: string
   volume: number
--- packages/payload/packages/stock/packages/market/packages/tzero/src/Payload/index.ts
+++ packages/payload/packages/stock/packages/market/packages/tzero/src/Payload/index.ts
@@ -1,3 +1,2 @@
-export * from './MarketdataDetailSnapshot'
 export * from './MarketdataSnapshot'
 export * from './PriceHistory'
