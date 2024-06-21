bf0d81d09c1fcc47dc876efdaa5b0454769bd393
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-21 12:19:01 -0500

    Use identity over casting

--- packages/payloadset/packages/xml/src/Diviner/spec/Diviner.spec.ts
+++ packages/payloadset/packages/xml/src/Diviner/spec/Diviner.spec.ts
@@ -1,10 +1,9 @@
 import { assertEx } from '@xylabs/assert'
-import { ApiCallResultSchema, ApiCallSchema, ApiCallWitness, ApiCallWitnessConfigSchema, isApiCallBase64Result } from '@xyo-network/api-call-witness'
+import { ApiCallResultSchema, ApiCallSchema, ApiCallWitness, ApiCallWitnessConfigSchema, isApiCallXmlResult } from '@xyo-network/api-call-witness'
 
-import { isXmlWithMeta, Xml } from '../../Payload'
+import { isXmlWithMeta } from '../../Payload'
 import { XmlSchema } from '../../Schema'
 import { XmlParsingDiviner } from '../Diviner'
-import { hasStringDataField } from '../lib'
 import { XmlParsingDivinerConfigSchema } from '../Schema'
 
 describe('XmlParsingDiviner', () => {
@@ -13,8 +12,7 @@ describe('XmlParsingDiviner', () => {
     it('parses XML from API response', async () => {
       const witness = await ApiCallWitness.create({
         account: 'random',
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        config: { accept: 'text/xml' as any, schema: ApiCallWitnessConfigSchema },
+        config: { accept: 'text/xml', schema: ApiCallWitnessConfigSchema },
       })
       const diviner = await XmlParsingDiviner.create({
         account: 'random',
@@ -22,14 +20,14 @@ describe('XmlParsingDiviner', () => {
       })
       const observations = await witness.observe([{ schema: ApiCallSchema, uri }])
       expect(observations).toBeArrayOfSize(1)
-      const observation = observations.filter(isApiCallBase64Result).find(hasStringDataField)
+      const observation = observations.find(isApiCallXmlResult)
       expect(observation).toBeDefined()
       expect(observation).toBeObject()
       expect(observation?.schema).toBe(ApiCallResultSchema)
       const xml = assertEx(observation, () => 'Error in observation')
       const results = await diviner.divine([xml])
       expect(results).toBeArrayOfSize(1)
-      const result: undefined | Xml = results.find(isXmlWithMeta)
+      const result = results.find(isXmlWithMeta)
       expect(result).toBeDefined()
       expect(result?.schema).toBe(XmlSchema)
       expect(result?.xml).toBeDefined()
