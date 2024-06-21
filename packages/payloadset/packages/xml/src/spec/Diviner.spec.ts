307ca3dc53b742d8e94ea575980ff4dea9ce5f48
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-06-21 06:38:30 -0500

    Add XML source for tests

--- packages/payloadset/packages/xml/src/spec/Diviner.spec.ts
+++ packages/payloadset/packages/xml/src/spec/Diviner.spec.ts
@@ -1,18 +1,18 @@
-import { ApiCallWitnessConfigSchema } from '../Config'
-import { ApiCallResultSchema, ApiCallSchema, isApiCallErrorResult } from '../Payload'
-import { ApiCallWitness } from '../Diviner'
+import { ApiCallResultSchema, ApiCallSchema, ApiCallWitness, ApiCallWitnessConfigSchema, isApiCallErrorResult } from '@xyo-network/api-call-witness'
 
-describe('CryptoWalletNftWitness', () => {
-  const uri = 'https://cnn.com'
+describe('XmlParsingDiviner', () => {
+  const uri = 'https://medium.com/feed/xyonetwork'
   describe('observe', () => {
     it('get code from contract', async () => {
       const witness = await ApiCallWitness.create({
         account: 'random',
-        config: { accept: 'application/json', schema: ApiCallWitnessConfigSchema },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        config: { accept: 'text/xml' as any, schema: ApiCallWitnessConfigSchema },
       })
       const observation = await witness.observe([{ schema: ApiCallSchema, uri }])
       expect(observation[0].schema).toBe(ApiCallResultSchema)
       expect(isApiCallErrorResult(observation[0].schema)).toBe(false)
+      const xml = Buffer.from((observation[0] as unknown as { data: string }).data).toString('utf8')
     })
   })
 })
