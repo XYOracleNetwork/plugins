362ea709c4a586f1c23198e2e61431a2c5d50050
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-07-18 05:34:29 -0500

    Add tests for emails

--- packages/payload/packages/email/src/spec/Payload.spec.ts
+++ packages/payload/packages/email/src/spec/Payload.spec.ts
@@ -1,7 +1,42 @@
 import {} from '../Payload.js'
 
 describe('Email', () => {
-  it('with valid email', () => {
-    //
+  describe('with valid email', () => {
+    const validEmails = [
+      'john.doe@example.com',
+      'jane_smith123@domain.net',
+      'contact@company.org',
+      'user+name@sub.domain.co',
+      'firstname.lastname@company-email.com',
+      'my.email@domain.edu',
+      'name@domain.travel',
+      'support@domain.tech',
+      'info@new-domain.info',
+      'example@domain.jobs',
+      'my-name@domain.gov',
+      'name@sub.domain.tld',
+    ]
+    it('with valid email', () => {
+      //
+    })
+  })
+  describe('with invalid email', () => {
+    const invalidEmails = [
+      'plainaddress',
+      '@missingusername.com',
+      'username@.com',
+      'username@domain..com',
+      'username@domain.c',
+      'username@domain,com',
+      'username@domain..co',
+      'user name@domain.com',
+      'username@domain@domain.com',
+      '.username@domain.com',
+      'username@domain.com.',
+      'username@domain..com',
+    ]
+    it('with valid email', () => {
+      //
+    })
   })
 })
