2548d8c25349d2b88606836db619b13a82f3383d
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-29 17:01:35 -0500

    Add new packages

---
+++ packages/payloadset/packages/xns/src/index.ts
@@ -0,0 +1,10 @@
+export * from '@xyo-network/xns-record-payload-plugins'
+import type { PayloadPluginFunc } from '@xyo-network/payload-plugin'
+import {} from '@xyo-network/xns-record-payload-plugins'
+
+export const XnsRecordPayloadPlugins: PayloadPluginFunc[] = [
+  // TODO: Exports from xns-record-payload-plugins
+]
+
+// eslint-disable-next-line import-x/no-default-export
+export default XnsRecordPayloadPlugins
