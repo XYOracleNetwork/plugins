e6cd5b2534e9b053510e263880154bd7f3636d55
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-08-30 12:22:07 -0500

    Fix reaching

--- packages/payload/packages/xns/plugins/record/src/DomainEstimate/DomainEstimate.ts
+++ packages/payload/packages/xns/plugins/record/src/DomainEstimate/DomainEstimate.ts
@@ -2,7 +2,7 @@ import type { BoundWitness } from '@xyo-network/boundwitness-model'
 import type { HashLeaseEstimate } from '@xyo-network/diviner-hash-lease'
 import type { WithOptionalMeta, WithSources } from '@xyo-network/payload-model'
 
-import type { DomainRegistrationLease } from '../DomainRegistration/DomainRegistrationLease.ts'
+import type { DomainRegistrationLease } from '../DomainRegistration/index.ts'
 
 export type DomainEstimate = [
   WithOptionalMeta<BoundWitness>,
