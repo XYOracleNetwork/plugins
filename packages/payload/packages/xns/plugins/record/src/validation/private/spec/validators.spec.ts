9ab0088ba6a5350294c4201e12de53eaa8c2e066
Matt Jones <885417+jonesmac@users.noreply.github.com>
2024-09-03 19:39:56 -0400

    validate reserved fragments and names

--- packages/payload/packages/xns/plugins/record/src/validation/private/spec/validators.spec.ts
+++ packages/payload/packages/xns/plugins/record/src/validation/private/spec/validators.spec.ts
@@ -2,7 +2,9 @@ import type { WithSources } from '@xyo-network/payload-model'
 
 import type { DomainRegistration } from '../../../DomainRegistration/index.ts'
 import { DomainRegistrationSchema } from '../../../DomainRegistration/index.ts'
-import { getDomainRegistrationReservedStringsValidator } from '../index.ts'
+import {
+  getDomainRegistrationReservedFragmentsValidator, getDomainRegistrationReservedNamesValidator, getDomainRegistrationReservedStringsValidator,
+} from '../index.ts'
 
 const baseDomainRegistrationFields: DomainRegistration = {
   domain: '',
@@ -16,23 +18,53 @@ describe('XNS Name Validators', () => {
   describe('Private Validators', () => {
     const cases = [
       {
+        getValidator: getDomainRegistrationReservedStringsValidator,
         name: 'ReservedStringsValidator',
-        reservedStrings: ['foo'],
+        reservedList: ['foo'],
         valid: ['bar'],
         invalid: ['foo'],
       },
       {
+        getValidator: getDomainRegistrationReservedStringsValidator,
         name: 'ReservedStringsValidator',
-        reservedStrings: [],
+        reservedList: [],
         valid: ['bar', 'foobar'],
+        invalid: [''],
+      },
+      {
+        getValidator: getDomainRegistrationReservedFragmentsValidator,
+        name: 'ReservedFragmentsValidator',
+        reservedList: ['foo'],
+        valid: ['bar'],
+        invalid: ['foobar', 'bar-foo'],
+      },
+      {
+        getValidator: getDomainRegistrationReservedFragmentsValidator,
+        name: 'ReservedFragmentsValidator',
+        reservedList: [],
+        valid: ['foobar', 'bar-foo'],
+        invalid: [''],
+      },
+      {
+        getValidator: getDomainRegistrationReservedNamesValidator,
+        name: 'ReservedNamesValidator',
+        reservedList: ['john doe'],
+        valid: ['john1'],
+        invalid: ['johndoe', 'doejohn'],
+      },
+      {
+        getValidator: getDomainRegistrationReservedNamesValidator,
+        name: 'ReservedNamesValidator',
+        reservedList: [],
+        valid: ['john', 'doe'],
         invalid: [''],
       },
     ]
 
     describe.each(cases)('$name', ({
-      valid, invalid, reservedStrings,
+      getValidator, valid, invalid, reservedList,
     }) => {
-      const validator = getDomainRegistrationReservedStringsValidator(reservedStrings)
+      const validator = getValidator(reservedList)
       describe('Valid', () => {
         it.each(valid)('should return true for %s', (domain) => {
           const domainRegistration: WithSources<DomainRegistration> = { ...baseDomainRegistrationFields, domain }
