945fc3d8ae0ba11da987887e30aa832f4dbc4e02
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-09-12 07:46:17 -0500

    Simplify method complexity via extracted helper

--- packages/payloadset/packages/payments/src/Discount/Diviner.ts
+++ packages/payloadset/packages/payments/src/Discount/Diviner.ts
@@ -69,35 +69,13 @@ export class PaymentDiscountDiviner<
     // If not all appraisals are found, return no discount
     if (appraisals.length !== termsAppraisals.length) return [{ ...NO_DISCOUNT, sources }] as TOut[]
 
-    // Parse discounts
-    const discountHashes = terms.discounts ?? []
-    if (discountHashes.length === 0) return [{ ...NO_DISCOUNT, sources }] as TOut[]
-
-    // Use the supplied payloads to find the discounts
-    const discounts = discountHashes.map(hash => hashMap[hash]).filter(exists).filter(isCoupon) as Coupon[]
-    // Find any remaining coupons from the archivist
-    if (discounts.length !== discountHashes.length) {
-      // Find remaining from discounts archivist
-      const discountsArchivist = await this.getDiscountsArchivist()
-      const foundDiscounts = await discountsArchivist.get(discountHashes)
-      discounts.push(...foundDiscounts.filter(isCouponWithMeta))
-    }
-    const discountsMap = await PayloadBuilder.toAllHashMap(discounts)
-    if (Object.keys(discountsMap).length === 0) return [{ ...NO_DISCOUNT, sources }] as TOut[]
-
-    // Add the found discounts to the sources
-    const foundDiscountsHashes = Object.keys(discountsMap) as Hash[]
-
     // Parse coupons
     const coupons = await this.getEscrowDiscounts(terms, hashMap)
-    // Log individual discounts that were not found
-    for (const hash of discountHashes) {
-      if (foundDiscountsHashes.includes(hash)) {
-        sources.push(hash)
-      } else {
-        console.warn(`Discount ${hash} not found for terms ${await PayloadBuilder.hash(terms)}`)
-      }
-    }
+    // Add the coupons that were found to the sources
+    // NOTE: Should we throw if not all coupons are found? Not all valid?
+    const couponHashes = await PayloadBuilder.hashes(coupons)
+    sources.push(...couponHashes)
+
     const validCoupons = await this.filterToSigned(coupons.filter(this.isCouponCurrent))
     if (validCoupons.length === 0) return [{ ...NO_DISCOUNT, sources }] as TOut[]
 
@@ -172,22 +150,16 @@ export class PaymentDiscountDiviner<
       const payloads = await discountsArchivist.get(missing)
       discounts.push(...payloads.filter(isCouponWithMeta))
     }
-    const discountsMap = await PayloadBuilder.toAllHashMap(discounts)
-    if (Object.keys(discountsMap).length === 0) return []
-
-    // Add the found discounts to the sources
-    const foundDiscountsHashes = Object.keys(discountsMap) as Hash[]
-
-    // Log individual discounts that were not found
-    for (const hash of hashes) {
-      if (!foundDiscountsHashes.includes(hash)) {
-        console.warn(`Discount ${hash} not found for terms ${await PayloadBuilder.hash(terms)}`)
+    // If not all discounts are found
+    if (discounts.length !== hashes.length) {
+      const termsHash = await PayloadBuilder.hash(terms)
+      const foundHashes = await PayloadBuilder.hashes(discounts)
+      // Log individual discounts that were not found
+      for (const hash of hashes) {
+        if (!foundHashes.includes(hash)) console.warn(`Discount ${hash} not found for terms ${termsHash}`)
       }
     }
-
-    // Parse coupons
-    const coupons = Object.values(discountsMap)
-    return coupons
+    return discounts
   }
 
   protected isCouponCurrent(coupon: Coupon): boolean {
