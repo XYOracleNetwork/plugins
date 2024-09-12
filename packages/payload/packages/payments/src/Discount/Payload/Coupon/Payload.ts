2094f71630b8608a733e71aaeb551078b951f72b
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-09-12 06:42:53 -0500

    Update coupon types and identities

--- packages/payload/packages/payments/src/Discount/Payload/Coupon/Payload.ts
+++ packages/payload/packages/payments/src/Discount/Payload/Coupon/Payload.ts
@@ -1,25 +1,39 @@
 import {
-  type FixedAmountCoupon, type FixedPercentageCoupon, isFixedAmountCoupon, isFixedAmountCouponWithMeta, isFixedAmountCouponWithSources, isFixedPercentageCoupon,
+  type FixedAmountCoupon, type FixedPercentageCoupon, type FixedPriceCoupon,
+  isFixedAmountCoupon, isFixedAmountCouponWithMeta, isFixedAmountCouponWithSources, isFixedPercentageCoupon,
   isFixedPercentageCouponWithMeta,
   isFixedPercentageCouponWithSources,
+  isFixedPriceCoupon,
+  isFixedPriceCouponWithSources,
 } from './Coupons/index.ts'
 
 /**
- * The result of a discount
+ * The type of coupons
  */
-export type Coupon = FixedAmountCoupon | FixedPercentageCoupon
+export type Coupon =
+  FixedAmountCoupon
+  | FixedPercentageCoupon
+  | FixedPriceCoupon
 
 /**
  * Identity function for determining if an object is an Coupon
  */
-export const isCoupon = (x?: unknown | null) => isFixedAmountCoupon(x) || isFixedPercentageCoupon(x)
+export const isCoupon = (x?: unknown | null) =>
+  isFixedAmountCoupon(x)
+  || isFixedPercentageCoupon(x)
+  || isFixedPriceCoupon(x)
 
 /**
  * Identity function for determining if an object is an Coupon with sources
  */
-export const isCouponWithSources = (x?: unknown | null) => isFixedAmountCouponWithSources(x) || isFixedPercentageCouponWithSources(x)
+export const isCouponWithSources = (x?: unknown | null) =>
+  isFixedAmountCouponWithSources(x)
+  || isFixedPercentageCouponWithSources(x)
+  || isFixedPriceCouponWithSources(x)
 
 /**
  * Identity function for determining if an object is an Coupon with meta
  */
-export const isCouponWithMeta = (x?: unknown | null) => isFixedAmountCouponWithMeta(x) || isFixedPercentageCouponWithMeta(x)
+export const isCouponWithMeta = (x?: unknown | null) =>
+  isFixedAmountCouponWithMeta(x)
+  || isFixedPercentageCouponWithMeta(x)
