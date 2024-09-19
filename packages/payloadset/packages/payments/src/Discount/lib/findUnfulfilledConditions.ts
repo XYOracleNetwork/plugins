a2e5b16936b73316b1eb88810c14a46f57189d93
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-09-19 08:31:34 -0500

    Separate coupons and conditions in function signature

--- packages/payloadset/packages/payments/src/Discount/lib/findUnfulfilledConditions.ts
+++ packages/payloadset/packages/payments/src/Discount/lib/findUnfulfilledConditions.ts
@@ -1,8 +1,9 @@
 import type { Hash } from '@xylabs/hex'
 import { PayloadBuilder } from '@xyo-network/payload-builder'
-import type { Payload, WithMeta } from '@xyo-network/payload-model'
-import type { Coupon } from '@xyo-network/payment-payload-plugins'
-import type { SchemaPayload } from '@xyo-network/schema-payload-plugin'
+import type { Payload } from '@xyo-network/payload-model'
+import {
+  type Condition, type Coupon, isCondition,
+} from '@xyo-network/payment-payload-plugins'
 import { isSchemaPayloadWithMeta } from '@xyo-network/schema-payload-plugin'
 import type { ValidateFunction } from 'ajv'
 import { Ajv } from 'ajv'
@@ -11,25 +12,26 @@ import { Ajv } from 'ajv'
 const ajv = new Ajv({ strict: false }) // Create the Ajv instance once
 const schemaCache = new Map() // Cache to store compiled validators
 
-export const areConditionsFulfilled = async (coupon: Coupon, payloads: Payload[]): Promise<boolean> =>
-  (await findUnfulfilledConditions(coupon, payloads)).length === 0
+export const areConditionsFulfilled = async (coupon: Coupon, conditions: Condition[] = [], payloads: Payload[] = []): Promise<boolean> =>
+  (await findUnfulfilledConditions(coupon, conditions, payloads)).length === 0
 
 // TODO: Should we separate conditions and payloads to prevent conflating data and "operands" (schemas to validate against data)?
 /**
  * Validates the conditions of a coupon against the provided payloads
  * @param coupon The coupon to check
+ * @param conditions The conditions associated with the coupon
  * @param payloads The associated payloads (containing the conditions and data to validate the conditions against)
  * @returns The unfulfilled condition hashes
  */
-export const findUnfulfilledConditions = async (coupon: Coupon, payloads: Payload[]): Promise<Hash[]> => {
+export const findUnfulfilledConditions = async (coupon: Coupon, conditions: Condition[] = [], payloads: Payload[] = []): Promise<Hash[]> => {
   const unfulfilledConditions: Hash[] = []
   // If there are no conditions, then they are fulfilled
   if (!coupon.conditions || coupon.conditions.length === 0) return unfulfilledConditions
-  const hashMap = await PayloadBuilder.toAllHashMap(payloads)
+  const hashMap = await PayloadBuilder.toAllHashMap([...conditions, ...payloads])
   // Find all the conditions
-  const conditions = coupon.conditions.map(hash => hashMap[hash]).filter(isSchemaPayloadWithMeta) as WithMeta<SchemaPayload>[]
+  const foundConditions = coupon.conditions.map(hash => hashMap[hash]).filter(isCondition)
   // Not all conditions were found
-  if (conditions.length !== coupon.conditions.length) {
+  if (foundConditions.length !== coupon.conditions.length) {
     const missing = coupon.conditions.filter(hash => !hashMap[hash])
     unfulfilledConditions.push(...missing)
     return unfulfilledConditions
