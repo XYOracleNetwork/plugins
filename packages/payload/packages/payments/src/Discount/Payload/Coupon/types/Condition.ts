338dadcb70fc7c1838803006aca88f9c82446767
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-12 06:52:21 -0600

    Add identity functions

--- packages/payload/packages/payments/src/Discount/Payload/Coupon/types/Condition.ts
+++ packages/payload/packages/payments/src/Discount/Payload/Coupon/types/Condition.ts
@@ -1,3 +1,4 @@
+import { AsObjectFactory } from '@xylabs/object'
 import { isPayloadOfSchemaType, isPayloadOfSchemaTypeWithSources } from '@xyo-network/payload-model'
 import { type SchemaPayload, SchemaSchema } from '@xyo-network/schema-payload-plugin'
 
@@ -10,8 +11,10 @@ export type Condition = SchemaPayload
  * Identity function for determining if an object is a Condition payload
  */
 export const isCondition = isPayloadOfSchemaType<Condition>(SchemaSchema)
+export const asCondition = AsObjectFactory.create(isCondition)
 
 /**
  * Identity function for determining if an object is a Condition payload with sources
- */
+*/
 export const isConditionWithSources = isPayloadOfSchemaTypeWithSources<Condition>(SchemaSchema)
+export const asConditionWithSources = AsObjectFactory.create(isConditionWithSources)
