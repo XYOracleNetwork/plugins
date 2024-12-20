d250f8f7cfcd5a03b674b93a1949c9d41bd6ade1
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-20 08:31:44 -0600

    Update test vectors and snapshots

--- packages/payload/packages/xns/plugins/record/src/diviner/lib/spec/parseEstimatesFromArray.spec.ts
+++ packages/payload/packages/xns/plugins/record/src/diviner/lib/spec/parseEstimatesFromArray.spec.ts
@@ -1,4 +1,6 @@
 import { matchers } from '@xylabs/vitest-matchers'
+import { HDWallet } from '@xyo-network/account'
+import { BoundWitnessBuilder } from '@xyo-network/boundwitness-builder'
 import type { Payload } from '@xyo-network/payload-model'
 import {
   describe, expect, it,
@@ -12,16 +14,20 @@ expect.extend(matchers)
 describe('parseEstimatesFromArray', () => {
   const estimateA: Estimate = [
     {
-      schema: 'network.xyo.boundwitness',
-      addresses: ['2486705072e91a1d80d97cdcccd5db47b584e4bd'],
+      addresses: ['ce5ba87678f8b5e2d0dbccf2fabff65f79191f73'],
       payload_hashes: [
-        'dcc7ab18c86d90311bb1b59361f6698e2714294a91aa3126c554aa8483a7ae37',
-        '06882b9889a7649d7172e96c2c5046d5472685cf893c2e38dacd7ef154d7808d',
+        '49699c9d1cfb1a8643037c9557123a9e643a3dd938f619af39b94966ecfdd6ff',
+        'af31f9d6afd8f7cbbe5c3310f2a2da9e1af31d1b7cf8dae7d9786848de1e5bd7',
       ],
-      payload_schemas: ['network.xyo.ns.domain.registration.lease', 'network.xyo.hash.lease.estimate'],
+      payload_schemas: [
+        'network.xyo.hash.lease.estimate',
+        'network.xyo.ns.domain.registration.lease',
+      ],
       previous_hashes: [null],
-      $signatures: ['6d1f0d8d175f0bce32e5e474d263b0c6a815b698563a2d761464acea411bace051fe0fe720905688df8debfb13eed2979fd22f39d07922f06c8f5bebdafa5bb1'],
-      $sourceQuery: 'd5cd65081af158789b481415644549d72cb6c5925ba68918c4ac55e92bf1f0e2',
+      schema: 'network.xyo.boundwitness',
+      $signatures: [
+        '2a59fea264bf71603f9908ac557cf5816ddd1898ce26a2cbb8081195cf5a19381bf42cc2e583a9a2a768dc324c6ac1344eb4ec06f41b19b06970734b30340226',
+      ],
     },
     {
       schema: 'network.xyo.hash.lease.estimate',
@@ -44,14 +50,20 @@ describe('parseEstimatesFromArray', () => {
   ]
   const estimateB: Estimate = [
     {
-      schema: 'network.xyo.boundwitness',
-      addresses: ['2486705072e91a1d80d97cdcccd5db47b584e4bd'],
+      addresses: ['9146761e29463d783e28161848feeeb43196227b'],
       payload_hashes: [
-        '90259bc9ecde9d4c0061bc92dbfcf32e0f7b2603e49443d9f3c67a6d7e82c7d5', '7037fc16e12c7cffec09ed1d2ef3f65737668864be38f4a1268026d17601b5ca'],
-      payload_schemas: ['network.xyo.ns.domain.registration.lease', 'network.xyo.hash.lease.estimate'],
+        'abb7ecb2cfc51e1a95f85477b3a4210a72886d5b22fcf32043736c48e2873f2c',
+        '3ed825d48507b899395ca943e1dd6ec9a7c948e1cd1573a6cfda66de83af4b11',
+      ],
+      payload_schemas: [
+        'network.xyo.hash.lease.estimate',
+        'network.xyo.ns.domain.registration.lease',
+      ],
       previous_hashes: [null],
-      $signatures: ['ec17e78d94d256a0e4e9877855d92ed7bca7f7685ac42b5df87e6395331726a24f6a9271f3f058e1d35e06a11022ff359ccddbfa243772a8e45addad967afb6f'],
-      $sourceQuery: '19b2b1e4e0e50747f2cb0134e61cda9523ff9f963a40f3fc9e8e9530834b360b',
+      schema: 'network.xyo.boundwitness',
+      $signatures: [
+        '91519424f721376751b19865433ff80e3d47b2a59be6f7478e7e003e0366ccc730c1dd7eeaa642ced9e78a40c5a5a3ab33044a6d620f5a4467dfbd0e2d9dc4d8',
+      ],
     },
     {
       schema: 'network.xyo.hash.lease.estimate',
@@ -80,6 +92,8 @@ describe('parseEstimatesFromArray', () => {
       [...estimateB, ...estimateA],
     ]
     it.each(cases)('parses estimates from array', async (...data) => {
+      const account = await HDWallet.random()
+      const [bw] = await new BoundWitnessBuilder().signer(account).payloads([estimateB[1], estimateB[2]]).build()
       const parsed = await parseEstimatesFromArray(data)
       expect(parsed).toMatchSnapshot()
     })
