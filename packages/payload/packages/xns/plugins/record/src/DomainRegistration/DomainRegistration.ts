7517b44cce0bff5596f66a76bb11a28eb4a09885
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-12-23 08:00:37 -0600

    createOptional for DomainRegistration identities

--- packages/payload/packages/xns/plugins/record/src/DomainRegistration/DomainRegistration.ts
+++ packages/payload/packages/xns/plugins/record/src/DomainRegistration/DomainRegistration.ts
@@ -1,4 +1,5 @@
 import type { Address } from '@xylabs/hex'
+import { AsObjectFactory } from '@xylabs/object'
 import type { Payload } from '@xyo-network/payload-model'
 import { isPayloadOfSchemaType, isPayloadOfSchemaTypeWithSources } from '@xyo-network/payload-model'
 
@@ -28,8 +29,12 @@ export type DomainRegistration = Payload<DomainRegistrationFields, DomainRegistr
  * Identity function for DomainRegistration payload
  */
 export const isDomainRegistration = isPayloadOfSchemaType<DomainRegistration>(DomainRegistrationSchema)
+export const asDomainRegistration = AsObjectFactory.create<DomainRegistration>(isDomainRegistration)
+export const asOptionalDomainRegistration = AsObjectFactory.createOptional<DomainRegistration>(isDomainRegistration)
 
 /**
  * Identity function for DomainRegistration payload with sources
  */
 export const isDomainRegistrationWithSources = isPayloadOfSchemaTypeWithSources<DomainRegistration>(DomainRegistrationSchema)
+export const asDomainRegistrationWithSources = AsObjectFactory.create<DomainRegistration>(isDomainRegistrationWithSources)
+export const asOptionalDomainRegistrationWithSources = AsObjectFactory.createOptional<DomainRegistration>(isDomainRegistrationWithSources)
