ed603a85b2b44649f0c9989bd880adad67b45f6b
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-23 09:17:11 -0500

    Use v4 schema

--- packages/payloadset/packages/crypto/packages/market/packages/uniswap/packages/v4/src/Config.ts
+++ packages/payloadset/packages/crypto/packages/market/packages/uniswap/packages/v4/src/Config.ts
@@ -1,8 +1,8 @@
-import type { UniswapCryptoMarketWitnessConfigSchema } from '@xyo-network/uniswap-crypto-market-payload-plugin'
+import type { UniswapV4CryptoMarketWitnessConfigSchema } from '@xyo-network/uniswap-crypto-market-payload-plugin'
 import type { WitnessConfig } from '@xyo-network/witness-model'
 import { TokenPairPoolKey } from './types/index.ts'
 
 export type UniswapV4CryptoMarketWitnessConfig = WitnessConfig<{
   poolKeys?: TokenPairPoolKey[]
-  schema: UniswapCryptoMarketWitnessConfigSchema
+  schema: UniswapV4CryptoMarketWitnessConfigSchema
 }>
