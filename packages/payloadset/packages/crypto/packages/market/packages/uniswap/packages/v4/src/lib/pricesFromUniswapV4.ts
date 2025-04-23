435e9145c10d1798fe2ff907cb1a5304842768f1
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-04-23 10:44:25 -0500

    Remove async

--- packages/payloadset/packages/crypto/packages/market/packages/uniswap/packages/v4/src/lib/pricesFromUniswapV4.ts
+++ packages/payloadset/packages/crypto/packages/market/packages/uniswap/packages/v4/src/lib/pricesFromUniswapV4.ts
@@ -5,5 +5,5 @@ import type { TokenPairPoolKey } from '../types/index.ts'
 import { priceFromUniswapV4 } from './priceFromUniswapV4.ts'
 
 export const pricesFromUniswapV4 = async (poolId: TokenPairPoolKey[], provider: Provider): Promise<UniswapCryptoPair[]> => {
-  return await Promise.all(poolId.map(async pool => priceFromUniswapV4(pool, provider)))
+  return await Promise.all(poolId.map(pool => priceFromUniswapV4(pool, provider)))
 }
