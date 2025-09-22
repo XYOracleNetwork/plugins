f22e5499635d9b57ca8e2c2c51f7acdf23e684f6
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-22 13:11:49 -0500

    Add XL1 to assets

--- packages/payload/packages/crypto/packages/market/packages/coingecko/src/lib/AssetSymbol.ts
+++ packages/payload/packages/crypto/packages/market/packages/coingecko/src/lib/AssetSymbol.ts
@@ -1,1 +1,1 @@
-export type AssetSymbol = 'btc' | 'eth' | 'xyo' | 'usd' | 'eur' | 'usdt' | 'usdc' | 'bnb' | 'xrp' | 'ada' | 'sol' | 'busd' | 'dot' | 'doge' | 'wbtc'
+export type AssetSymbol = 'btc' | 'eth' | 'xyo' | 'xl1' | 'usd' | 'eur' | 'usdt' | 'usdc' | 'bnb' | 'xrp' | 'ada' | 'sol' | 'busd' | 'dot' | 'doge' | 'wbtc'
